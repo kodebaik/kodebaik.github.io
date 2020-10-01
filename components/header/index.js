aaf0f003eb004ad272caea667cafa86cf6fae095
andi hakim arif <andi.hakim.arif@gmail.com>
2020-10-01 22:16:26 +0700

    header layout max width (#4)

--- components/header/index.js
+++ components/header/index.js
@@ -3,8 +3,10 @@ import styles from './header.module.css'
 export default function Header() {
   return (
     <div className={styles.container}>
-      <div className={styles.containerLeft}>
-        <img src='/images/logo.svg' alt='kodeBaik' />
+      <div className={styles.innerContainer}>
+        <div className={styles.containerLeft}>
+          <img src='/images/logo.svg' alt='kodeBaik' />
+        </div>
       </div>
     </div>
   )
